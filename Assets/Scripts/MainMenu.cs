3b641b2314d41cdb70f03d72f1df97fa563c3323
Lilian Pouvreau <64379456+Banalian@users.noreply.github.com>
2022-09-10 18:55:27 -0400

    feat(credit): add group name to main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,15 +7,23 @@ public class MainMenu : MonoBehaviour
     private Button startButton;
     private Button quitButton;
 
+    private Label madeByLabel;
+
     public void Awake()
     {
         VisualElement document = GetComponentInChildren<UIDocument>().rootVisualElement;
 
         startButton = document.Q<Button>("Start");
         quitButton = document.Q<Button>("Quit");
+        madeByLabel = document.Q<Label>("MadeBy");
 
         startButton.clicked += () => SceneManager.LoadScene("Pacman");
         quitButton.clicked += () => Application.Quit();
-        
+
+        madeByLabel.text = "Made by: \n" +
+            "Lilian Pouvreau POUL06120000\n" +
+            "Benoît Caudron CAUB22020107\n" +
+            "Mathis Ronzon RONM16090000\n" +
+            "Tristan Guichard GUIT13020005";
     }
 }
